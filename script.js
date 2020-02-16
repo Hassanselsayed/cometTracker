581f94834d3e8dab1a091a57346e7bdc11d69549
Hassanselsayed <hsaadelsayed@gmail.com>
2020-02-15 19:08:12 -0500

    namespacing

--- script.js
+++ script.js
@@ -1,89 +1,88 @@
+// app object
 const cometApp = {};
 
-
 // using regex to add commas to numbers - imported from stackoverflow https://stackoverflow.com/questions/3883342/add-commas-to-a-number-in-jquery
-function commaSeparateNumber(val) {
+cometApp.commaSeparateNumber = function(val) {
     while (/(\d+)(\d{3})/.test(val.toString())) {
         val = val.toString().replace(/(\d+)(\d{3})/, '$1' + ',' + '$2');
     }
     return val;
-}
+};
 
-// event listeners
-$('select').change(function () {
+// select element listener
+cometApp.selectListener = function() {
     $('.closestObjFlex').empty();
     const $userChoice = $(this).children("option:selected");
     const $userChoiceVal = $userChoice.val();
 
+    cometApp.ajaxCall($userChoice, $userChoiceVal);
+};
+
+// callback function to be used in the .filter method
+cometApp.findIndex = function(value, index) {
+    return index <= 2;
+};
+
+// AJAX call function
+cometApp.ajaxCall = function(planetName, planetValue) {
+    // start of AJAX call
     $.ajax({
         url: 'https://ssd-api.jpl.nasa.gov/cad.api',
         method: 'GET',
         dataType: 'json',
         data: {
             fullname: 'true',
-            body: $userChoiceVal
+            body: planetValue
         } 
-    }).then(function (result) {
-
+    }).then(function(result) {
         console.log(result);
         if (result.count === '0') {
-            console.log(`sorry, no comets around ${$userChoice.text()} now`);
+            console.log(`sorry, no comets around ${planetName.text()} now`);
         } else {
-            function findIndex(value, index){
-                return index <= 2
-            };
-    
-            const $filteredObjects = result.data.filter(findIndex);
-    
-            // append h3 div 
-            const h3ToAppend = `<h3>Closest ${$filteredObjects.length} objects</h3>`;
-            $('.closestObjFlex').append(h3ToAppend);
-    
-            // find first 3 objects from search 
-            $filteredObjects.forEach(function(currentVal, i) {
-                console.log($filteredObjects[i][4]);
-                const $convertedDistance = $filteredObjects[i][4] * 149597871;
 
-                const $commaSeperatedDistance = commaSeparateNumber($convertedDistance.toFixed(0));
-                console.log($commaSeperatedDistance);
+            // creating a new filtered array using the .filter method on the result.data array to get the first 3 values only
+            const $filteredObjects = result.data.filter(cometApp.findIndex);
+
+            // appending h3 to container div
+            if (result.count === '1') {
+                const h3ToAppend = `<h3>Closest ${$filteredObjects.length} object</h3>`;
+                $('.closestObjFlex').append(h3ToAppend);
+            } else {
+                const h3ToAppend = `<h3>Closest ${$filteredObjects.length} objects</h3>`;
+                $('.closestObjFlex').append(h3ToAppend);
+            }
+
+            // looping over the new filtered array using the .forEach method
+            $filteredObjects.forEach(function(currentVal, i) {
+                const $convertedDistance = $filteredObjects[i][4] * 149597871;
+                const $commaSeperatedDistance = cometApp.commaSeparateNumber($convertedDistance.toFixed(0));
                 
-    
                 const htmlToAppend = `
                 <ul>
                     <li>Name: ${$filteredObjects[i][11]}</li>
                     <li>Time of closest approach: ${$filteredObjects[i][3]}</li>
-                <li>Distance from ${$userChoice.text()}: ${$commaSeperatedDistance} km </li>
+                    <li>Distance from ${planetName.text()}: ${$commaSeperatedDistance} km </li>
                 </ul>
                 `;
                 $('.closestObjFlex').append(htmlToAppend);
-            });          
+            // end of forEach method
+            });
+        // end of else condition
         }
-
-        })
+    // end of .then method
     });
+// end of AJAX call function
+};
 
-
+cometApp.init = function () {
+    $('select').change(cometApp.selectListener);
+}
     
-    
-$(document).ready(function () {
+$(() => {
+    cometApp.init();
 });
 
-
-
-// $('.closestObjFlex').append(`
-// <h3>Closest ${$userChoiceArray.length} objects</h3>
-// <ul>
-//     <li>Name: ${$userChoiceArray[0][11]}</li>
-//     <li>Time of closest approach: ${$userChoiceArray[0][3]}</li>
-//     <li>Distance from ${$userChoice.text()}: ${$userChoiceArray[0][4]} astronomical unit</li>
-// </ul>
-// <ul>
-//     <li>Name: ${$userChoiceArray[1][11]}</li>
-//     <li>Time of closest approach: ${$userChoiceArray[1][3]}</li>
-//     <li>Distance from ${$userChoice.text()}: ${$userChoiceArray[1][4]} astronomical unit</li>
-// </ul>                <ul>
-//     <li>Name: ${$userChoiceArray[2][11]}</li>
-//     <li>Time of closest approach: ${$userChoiceArray[2][3]}</li>
-//     <li>Distance from ${$userChoice.text()}: ${$userChoiceArray[2][4]} astronomical unit</li>
-// </ul>
-// `);
+// invoiceApp.init = () => {
+//     $('ol').on('click', '#remove', invoiceApp.removeItemListener);
+//     $('form.addingItem').on('submit', invoiceApp.formListener);
+// };
